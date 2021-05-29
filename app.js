78a20662e5a85dbcd9f0852b7641bebd1d2ec4de
Wisterleys Sousa <wister.sousa2@gmail.com>
2021-05-29 16:19:37 -0300

    Server express criado e funcionando primeira rota

---
+++ app.js
@@ -0,0 +1,14 @@
+const Express = require("express");
+const app = Express();
+const os = require('os');
+const port = 80
+const object = os.networkInterfaces();
+//Busca IP
+let array=[];
+for (const key in object) {
+    array.push(key)
+}
+app.get("/",(req,res)=>{
+    res.send("Rodando");
+})
+app.listen(port,object[array[0]][1].address,()=>console.log(`Server rodando no IP ${object[array[0]][1].address} porta:${port}`))
